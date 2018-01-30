92b1635a58c74d3d92c2a0bfc2151c8931db88e4
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-30 09:49:22 -0600

    Introductory to the user, I made a helper function were Type of gas, Price, and Gallons are display to users.

--- Main.java
+++ Main.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void LoadPrice(){
+        System.out.println("Type\t\tPrice\tGallons");
+        System.out.println("Regular\t\t2.00\t5000");
+        System.out.println("Mid-Grade\t2.00\t5000");
+        System.out.println("Premium\t\t2.00\t5000");
+
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
         System.out.println("\nWELCOME to Violet Flowers Gas Station!!\n");
+        LoadPrice();
+        System.out.println("\n[1]Pre Pay or [2]Pay After");
     }
 }

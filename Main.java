309018bb6f98892dc982510cc1f022fae0ae902a
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-31 14:54:48 -0600

    Made a method that controls the only two ways to pay, which are pre-pay and pay-after, therefore deleted LoadPrice method.

--- Main.java
+++ Main.java
@@ -4,18 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void LoadPrice(){
-        System.out.println("Type\t\tPrice\tGallons");
-        System.out.println("Regular\t\t2.00\t5000");
-        System.out.println("Mid-Grade\t2.00\t5000");
-        System.out.println("Premium\t\t2.00\t5000");
-
+    public static void Pay(Integer PayType, String GasType, Double Amount){
+        if (PayType.equals(1)){
+            GasPump paying = new GasPump(GasType);
+            System.out.println(paying.prePay(GasType, Amount));
+        } else if(PayType.equals(2)){
+            GasPump paying = new GasPump(GasType);
+            System.out.println(paying.payAfter(GasType, Amount));
+        }
     }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("\nWELCOME to Violet Flowers Gas Station!!\n");
-        LoadPrice();
-        System.out.println("\n[1]Pre Pay or [2]Pay After");
+        System.out.println("\nWELCOME to Lizeth B Gas Station!!\n");
+        System.out.println("What would you like today:\n--regular\n--mid-grade\n--premium\n");
+        String GasType = reader.nextLine();
+        System.out.println("[1]Pre Pay or [2]Pay After");
+        Integer PayType = reader.nextInt();
+
+
+        if (PayType.equals(1)){
+            System.out.println("How much would you like to get?");
+            Double money = reader.nextDouble();
+            Pay(PayType, GasType, money);
+        }
+        else if (PayType.equals(2)){
+            System.out.println("How much would you like to get?");
+            Double money = reader.nextDouble();
+            Pay(PayType, GasType, money);
+        }
     }
 }

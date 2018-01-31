02e916def7cffc2090c2262d350900ccaec66679
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-31 14:59:21 -0600

    Made two methods, pre-pay and pay-after, that are called in main.

--- Gas.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class Gas {
-    public cal_money(gallons, price){}
-    
-}
--- GasPump.java
+++ GasPump.java
@@ -1,4 +1,34 @@
 package com.company;
 
 public class GasPump {
+    //Instance variables//
+    public String typeOfGas;
+
+    public GasPump(String typeOfGas) {
+        this.typeOfGas = typeOfGas;
+    }
+
+    public double prePay(String typeofGas, Double money) {
+        Double gallons = 0.0;
+        if (typeOfGas.equals("regular")) {
+            gallons = (money / 2.07);
+        } else if (typeOfGas.equals("mid-grade")) {
+            gallons = (money / 2.18);
+        } else if (typeOfGas.equals("premium")) {
+            gallons = (money / 2.23);
+        }
+        return Math.round(gallons);
+    }
+
+    public double payAfter(String typeOfGas, Double gallons) {
+        Double money = 0.0;
+        if (typeOfGas.equals("regular")) {
+            money = (gallons * 2.09);
+        } else if (typeOfGas.equals("mid-grade")) {
+            money = (gallons * 2.19);
+        } else if (typeOfGas.equals("premium")) {
+            money = (gallons * 2.29);
+        }
+        return Math.round(money);
+    }
 }
